da2c70ed87eef3d0dffe8a2f87619dcd0f4acfbe
reyhanfirdaus10 <rey130499@gmail.com>
2023-07-16 04:02:37 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#14b8a6",
+        primary: "#1e3a8a",
         secondary: "#64748b",
         dark: "#0f172a",
       },
