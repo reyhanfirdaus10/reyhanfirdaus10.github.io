47482d7ffcfa2118bd00f7f75cbf46b74c945f51
reyhanfirdaus10 <rey130499@gmail.com>
2023-07-15 21:50:55 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
 };
