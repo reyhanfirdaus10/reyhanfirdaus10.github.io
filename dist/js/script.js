be5b958c21d459e3cd9a9d2b7e818a226c3f7fd6
reyhanfirdaus10 <rey130499@gmail.com>
2023-07-20 18:33:52 +0700

    update fitures

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,3 +17,17 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbyBzqZ22bwc1KFsCRRZferjSXawxp20d7x-e8x9_swQGkeqqLpQfiiG-veZjLRZ4TWl/exec";
+const form = document.forms["porto-tailwind-form"];
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
